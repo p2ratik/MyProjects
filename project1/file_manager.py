af18f7001af7f58618efa1769e6e83d75d0034f9
Pratik <jaithalal680@gmail.com>
2025-05-09 12:04:44 +0530

    The file checker function has been updated and modified

--- project1/file_manager.py
+++ project1/file_manager.py
@@ -5,29 +5,18 @@ def file_ext(x):
     y=x.split(".")
     return y[1]
 def file_checker(name):
-    flag=0
-    for ch in name:
-        if(ch=='.'):
-            flag=1
-            break
-        else:
-            flag=0
-    if(flag==0):
-        return False
+    if(os.path.isfile(name) ):
+        return True
     else:
-        return True                
+        return False      
 list_of_only_files=list(filter(file_checker,list_off_files)) #Returns only the file that are files and no directory
 list_of_filextension=list(map(file_ext,list_of_only_files)) #Retunr st the extention /type of files
-print(list_of_filextension)
-set1=set(list_of_filextension)
-set2=set(list_off_files)
-print(set1)
+set1=set(list_of_filextension) #Set of file extensions
 for ext in set1:
-     if(os.path.isdir(ext)):
+     if(os.path.isdir(ext)): #Checks whether the directitory of the file extension already exists or not
          print(" ")
      else:
          os.mkdir(ext)    
-
 for files in list_of_only_files:
      x=files.split(".")
      a=x[1] 
